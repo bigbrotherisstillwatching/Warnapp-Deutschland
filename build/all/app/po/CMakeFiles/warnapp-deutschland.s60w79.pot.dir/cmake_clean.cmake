95ab255b02ff2501febf3feb41416664d32fab29
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2023-03-12 09:23:27 +0100

    Remove build dir from repo

--- build/all/app/po/CMakeFiles/warnapp-deutschland.s60w79.pot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/warnapp-deutschland.s60w79.pot"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/warnapp-deutschland.s60w79.pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
